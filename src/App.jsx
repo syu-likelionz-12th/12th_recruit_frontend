7c1073fcdeb082449df324e5f9b3e89338b51fc1
sungjun4403 <geulligu89@naver.com>
2024-01-19 15:01:17 +0900

    🔨 : merging components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import BenefitLists from "./components/BenefitLists";
 import CounterComponent from "./components/CounterComponent";
 import Introduce from "./components/introduce";
-
+import LikelionHeader from "./components/LikelionHeader";
 
 function App() {
   return (
   <main>
-    
-  <Introduce/>
-  <CounterComponent/>
-  <BenefitLists />
+  <LikelionHeader />
+  {/* <Introduce /> */}
+  {/* <CounterComponent/> */}
+  {/* <BenefitLists /> */}
     
   </main>
   );
